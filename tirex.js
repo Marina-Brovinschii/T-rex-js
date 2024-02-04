32b1190fcdb5d2dccc4a0561b0dfe74f4cc08fcf
Marina-Brovinschii <141269694+Marina-Brovinschii@users.noreply.github.com>
2024-02-04 12:21:34 +0200

    T-rex-js-array

---
+++ tirex.js
@@ -0,0 +1,43 @@
+let gameMap = [0, 2, 0, 0, 1, 0, 2, 0, 2, 0]
+
+let ti = 4
+let dir="left"
+
+
+function drawMap() {
+    m.innerHTML=``
+    for(let i=0; i<gameMap.length; i++) {
+        if (gameMap[i] ==0) {
+            m.innerHTML+=`<div></div>`
+        }else if (gameMap[i] == 1) {
+            m.innerHTML += `<div class="t-rex ${dir}"></div>`
+        }else if (gameMap[i] == 2) {
+            m.innerHTML += `<div class="cactus ${dir}"></div>`
+        }
+    }
+}
+
+drawMap()
+
+function action() {
+    switch(event.key) {
+        case "ArrowLeft": 
+            if (ti>0) {
+                gameMap[ti] = 0
+                ti--
+                gameMap[ti] = 1 
+                dir ="left"
+                drawMap()
+            }
+        break
+        case "ArrowRight": 
+            if (ti<9) {
+                gameMap[ti]=0
+                ti++
+                gameMap[ti]=1
+                dir ="right" 
+                drawMap()
+            }
+        break
+    }
+}
